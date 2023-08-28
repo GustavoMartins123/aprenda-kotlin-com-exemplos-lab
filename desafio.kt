fcc93e6787db7735e3ebc55fae62dfdbf1b4333a
Gustavo Henrique Martins <99695758+GustavoMartins123@users.noreply.github.com>
2023-08-28 20:20:20 -0300

    Initial commit

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,47 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { 
+    BASICO, 
+    INTERMEDIARIO, 
+    AVANCADO
+}
 
-class Usuario
+enum class Type{
+    MOBILE,
+    WINDOWS,
+    WEB
+}
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+enum class SubType{
+    ANDROID,
+    IOS,
+    HTML,
+    CSHARP,
+    JAVA
+}
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Usuario(var nome: String)
 
-    val inscritos = mutableListOf<Usuario>()
+data class ConteudoEducacional(var nome: String, val duracao: Int, val nivel: Nivel)
+
+data class Curso(val nome: String, val Categoria: Type, val Conteudo: SubType, val nivel: Nivel)
+
+data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>) {
+
+    var inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+        //TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    //TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
+    //TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val conteudo = ConteudoEducacional("Introdução à Programação", 60, Nivel.BASICO)
+    val curso = Curso("Curso de Android", Type.MOBILE, SubType.ANDROID, Nivel.INTERMEDIARIO)
+
+    println("Conteúdo: ${conteudo.nome}, Duração: ${conteudo.duracao}, Nível: ${conteudo.nivel}")
+    println("Curso: ${curso.nome}, Categoria: ${curso.Categoria}, Conteudo: ${curso.Conteudo}, Nível: ${curso.nivel}")
 }
